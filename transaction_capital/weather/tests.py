98d58a747d11eb4785a0a695c515d8892bb3c813
reez0 <riyaazfakier2@gmail.com>
2021-03-01 11:33:45 +0200

    Removed broken tests

--- transaction_capital/weather/tests.py
+++ transaction_capital/weather/tests.py
@@ -1,16 +1,2 @@
-from django.test import TestCase
-from django.urls import reverse
-from rest_framework import status
-from rest_framework.test import APITestCase
-import requests
-from .models import APIKeys
 
-class WeatherAPITests(APITestCase):
-    def test_retrieve_report(self):
-        url = reverse('weather_report')
-        data = {'city': 'Johannesburg'}
-        apikey = APIKeys(key_name='openweathermap.org',key_value='a15fafa5e9a0844798797ca91d98a2bd')
-        apikey.save()
-        response = self.client.post(url, data, format='json')
-        self.assertEqual(response.status_code, 200)
 
