a673a9507ca1c60ae5ab91978bf7dd6fcc92b8ef
reez0 <riyaazfakier2@gmail.com>
2021-03-01 13:17:36 +0200

    Fixed urls

--- transaction_capital/transaction_capital/urls.py
+++ transaction_capital/transaction_capital/urls.py
@@ -1,16 +1,17 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from django.conf.urls import url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from weather.views import WeatherView
 
 urlpatterns = [
+    path('', WeatherView.as_view(), name="default"),
     path('admin/', admin.site.urls),
     path('weather/', include('weather.urls')),
-
 ]
 
 urlpatterns += staticfiles_urlpatterns()
 
-urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
